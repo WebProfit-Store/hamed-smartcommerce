956f753fbc1b59510a9a7ece00df0dd875b6f2d8
WebProfit-Store <diabyhamed770@gmail.com>
2026-05-24 15:08:56 +0000

    Update page.tsx

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,9 +1,64 @@
-export default function AdminDashboard() {
+export default function AdminDashboardPage() {
+
   return (
-    <div className="p-10 text-white">
-      <h1 className="text-4xl font-bold">
-        Admin Dashboard
-      </h1>
+    <div>
+
+      <div className="mb-10">
+
+        <h1 className="text-4xl font-bold">
+          Dashboard
+        </h1>
+
+        <p className="text-zinc-400 mt-3">
+          Vue globale de votre business.
+        </p>
+
+      </div>
+
+      <div className="grid md:grid-cols-4 gap-6">
+
+        <div className="bg-zinc-900 border border-zinc-800 rounded-3xl p-6">
+          <p className="text-zinc-400">
+            Revenus
+          </p>
+
+          <h2 className="text-3xl font-bold mt-3">
+            2 450 000 FCFA
+          </h2>
+        </div>
+
+        <div className="bg-zinc-900 border border-zinc-800 rounded-3xl p-6">
+          <p className="text-zinc-400">
+            Commandes
+          </p>
+
+          <h2 className="text-3xl font-bold mt-3">
+            325
+          </h2>
+        </div>
+
+        <div className="bg-zinc-900 border border-zinc-800 rounded-3xl p-6">
+          <p className="text-zinc-400">
+            Clients
+          </p>
+
+          <h2 className="text-3xl font-bold mt-3">
+            1 204
+          </h2>
+        </div>
+
+        <div className="bg-zinc-900 border border-zinc-800 rounded-3xl p-6">
+          <p className="text-zinc-400">
+            Conversion
+          </p>
+
+          <h2 className="text-3xl font-bold mt-3">
+            8.4%
+          </h2>
+        </div>
+
+      </div>
+
     </div>
   );
 }
