bb4761e9ead0a94a1231c6915a199b92b7172385
WebProfit-Store <diabyhamed770@gmail.com>
2026-05-24 12:59:40 +0000

    Create products.ts

---
+++ src/data/products.ts
@@ -0,0 +1,27 @@
+export const products = [
+
+  {
+    id: "1",
+    name: "iPhone 15 Pro",
+    price: 950000,
+    image:
+      "https://images.unsplash.com/photo-1695048133142-1a20484d2569",
+  },
+
+  {
+    id: "2",
+    name: "Nike Air Max",
+    price: 120000,
+    image:
+      "https://images.unsplash.com/photo-1542291026-7eec264c27ff",
+  },
+
+  {
+    id: "3",
+    name: "Casque Gaming RGB",
+    price: 45000,
+    image:
+      "https://images.unsplash.com/photo-1585298723682-7115561c51b7",
+  },
+
+];
