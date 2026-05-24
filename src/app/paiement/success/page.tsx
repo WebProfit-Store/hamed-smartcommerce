b8f53b943258acefd9c602d6d1be1023f9d35f2b
WebProfit-Store <diabyhamed770@gmail.com>
2026-05-24 13:30:34 +0000

    Create page.tsx

---
+++ src/app/paiement/success/page.tsx
@@ -0,0 +1,19 @@
+export default function PaymentSuccessPage() {
+  return (
+    <main className="min-h-screen bg-black text-white flex items-center justify-center">
+
+      <div className="text-center">
+
+        <h1 className="text-5xl font-bold text-green-500">
+          Paiement réussi
+        </h1>
+
+        <p className="mt-6 text-zinc-400">
+          Votre commande a été confirmée.
+        </p>
+
+      </div>
+
+    </main>
+  );
+}
